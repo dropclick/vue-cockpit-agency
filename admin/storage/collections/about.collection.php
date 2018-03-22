3b2bf308dd87bb3f3dace9648b667a7dbb994680
Sven Petersen <s.petersen@webever.de>
2018-03-22 23:26:32 +0100

    about collection from cockpit

---
+++ admin/storage/collections/about.collection.php
@@ -0,0 +1,123 @@
+<?php
+ return array (
+  'name' => 'about',
+  'label' => 'About',
+  '_id' => 'about5ab428f22a429',
+  'fields' => 
+  array (
+    0 => 
+    array (
+      'name' => 'date',
+      'label' => 'Date',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    1 => 
+    array (
+      'name' => 'title',
+      'label' => 'Title',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    2 => 
+    array (
+      'name' => 'description',
+      'label' => 'Description',
+      'type' => 'textarea',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    3 => 
+    array (
+      'name' => 'image',
+      'label' => 'Image',
+      'type' => 'image',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    4 => 
+    array (
+      'name' => 'position',
+      'label' => 'Position',
+      'type' => 'select',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+        'options' => 'left, right',
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+  ),
+  'sortable' => true,
+  'in_menu' => true,
+  '_created' => 1521756402,
+  '_modified' => 1521757062,
+  'color' => '',
+  'acl' => 
+  array (
+  ),
+  'rules' => 
+  array (
+    'create' => 
+    array (
+    ),
+    'read' => 
+    array (
+    ),
+    'update' => 
+    array (
+    ),
+    'delete' => 
+    array (
+    ),
+  ),
+);
