a7095443a1d3f26b0b0969c4ab98333c34cffb16
Sven Petersen <s.petersen@webever.de>
2018-03-22 23:55:06 +0100

    partners from cockpit

---
+++ admin/storage/collections/partners.collection.php
@@ -0,0 +1,86 @@
+<?php
+ return array (
+  'name' => 'partners',
+  'label' => 'Partners',
+  '_id' => 'partners5ab43399e7461',
+  'fields' => 
+  array (
+    0 => 
+    array (
+      'name' => 'name',
+      'label' => 'Name',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    1 => 
+    array (
+      'name' => 'url',
+      'label' => 'URL',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    2 => 
+    array (
+      'name' => 'logo',
+      'label' => 'Logo',
+      'type' => 'image',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+  ),
+  'sortable' => true,
+  'in_menu' => true,
+  '_created' => 1521759129,
+  '_modified' => 1521759129,
+  'color' => '',
+  'acl' => 
+  array (
+  ),
+  'rules' => 
+  array (
+    'create' => 
+    array (
+    ),
+    'read' => 
+    array (
+    ),
+    'update' => 
+    array (
+    ),
+    'delete' => 
+    array (
+    ),
+  ),
+);
