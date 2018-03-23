13407d2590160d19484f226b968d5ac45c0d9f53
Sven Petersen <s.petersen@webever.de>
2018-03-23 16:04:19 +0100

    additional contact section fields in cockpit

--- admin/storage/regions/contact.region.php
+++ admin/storage/regions/contact.region.php
@@ -41,15 +41,132 @@
       array (
       ),
     ),
+    2 => 
+    array (
+      'name' => 'name',
+      'label' => 'Name',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    3 => 
+    array (
+      'name' => 'email',
+      'label' => 'E-Mail',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    4 => 
+    array (
+      'name' => 'phone',
+      'label' => 'Phone',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    5 => 
+    array (
+      'name' => 'message',
+      'label' => 'Message',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    6 => 
+    array (
+      'name' => 'button',
+      'label' => 'Button',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    7 => 
+    array (
+      'name' => 'background',
+      'label' => 'Background',
+      'type' => 'image',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
   ),
   'template' => '',
   'data' => 
   array (
     'title' => 'Contact us',
     'description' => 'Lorem ipsum dolor sit amet consectetur.',
+    'name' => 'Your Name *',
+    'email' => 'Your E-mail *',
+    'phone' => 'Your Phone *',
+    'message' => 'Your Message',
+    'button' => 'Send Message',
+    'background' => 
+    array (
+      'path' => '/admin/storage/uploads/2018/03/23/5ab50f952ef4amap-image.png',
+    ),
   ),
   '_created' => 1521795778,
-  '_modified' => 1521796991,
+  '_modified' => 1521817151,
   'description' => '',
   'acl' => 
   array (
