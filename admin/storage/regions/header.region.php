ea390c0d19f5c575a092518201f1f207737519f3
Sven Petersen <s.petersen@webever.de>
2018-03-23 15:35:14 +0100

    header from cockpit

---
+++ admin/storage/regions/header.region.php
@@ -0,0 +1,98 @@
+<?php
+ return array (
+  'name' => 'header',
+  'label' => 'Header',
+  '_id' => 'header5ab50d81f3f8f',
+  'fields' => 
+  array (
+    0 => 
+    array (
+      'name' => 'headline',
+      'label' => 'Headline',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    1 => 
+    array (
+      'name' => 'subline',
+      'label' => 'Subline',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    2 => 
+    array (
+      'name' => 'button',
+      'label' => 'Button',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    3 => 
+    array (
+      'name' => 'background',
+      'label' => 'Background',
+      'type' => 'image',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+  ),
+  'template' => '',
+  'data' => 
+  array (
+    'headline' => 'It\'s nice to meet you',
+    'subline' => 'Welcome to our studio!',
+    'button' => 'Tell me more',
+    'background' => 
+    array (
+      'path' => '/admin/storage/uploads/2018/03/23/5ab50e04ad12fheader-bg.jpg',
+    ),
+  ),
+  '_created' => 1521814913,
+  '_modified' => 1521815649,
+  'description' => '',
+  'acl' => 
+  array (
+  ),
+);
